7a507411d0547c84cf86e8f217346daa31c8b4b6
UdayMalviya <udaymalviya521@gmail.com>
2025-08-12 13:42:07 +0530

    adding better comments

--- chat.py
+++ chat.py
@@ -4,14 +4,14 @@ from langchain_google_genai import  ChatGoogleGenerativeAI
 from chat import embeddings
 
 
-# 2. Load FAISS vector store
+# Load FAISS vector store
 vector_store = FAISS.load_local(
     "faiss_store",
     embeddings,
     allow_dangerous_deserialization=True
 )
 
-# 3. Initialize Gemini chat model
+#  Initialize Gemini chat model
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",  
     temperature=0.2
@@ -20,18 +20,18 @@ llm = ChatGoogleGenerativeAI(
 
 print(" MindCoders Chatbot — type 'exit' to quit")
 
-# 4. Chat loop
+# Chat loop
 while True:
-    query = input("\nYou: ")
-    if query.lower() in ["exit", "quit"]:
+    query = input("\nYou: ") # User query
+    if query.lower() in ["exit", "quit"]: # to exit the loop
         print(" Goodbye!")
         break
 
     # Retrieve top documents
-    docs = vector_store.similarity_search(query, k=3)
-    context = "\n\n".join([doc.page_content for doc in docs])
+    docs = vector_store.similarity_search(query, k=3) # change K if you want to increase or decrease the similarity search instance
+    context = "\n\n".join([doc.page_content for doc in docs]) # final data going to pass on to generator (i.e.) model
 
-    # Build prompt
+    # System prompt can be modified according to need.
     prompt = f"""
     You are a helpful assistant for MindCoders training center.
     Use the following context to answer the question.
@@ -40,9 +40,9 @@ while True:
     {context}
 
     Question:
-    {query}
+    {query} 
     """
 
     # Get answer from Gemini
-    response = llm.invoke(prompt)
+    response = llm.invoke(prompt) 
     print("\n:", response.content)
--- loader.py
+++ loader.py
@@ -16,10 +16,10 @@ loader = WebBaseLoader([
             'https://learning.mindcoders.in/course/data_analitics',
             'https://learning.mindcoders.in/course/back_end',
             'https://learning.mindcoders.in/course/react'
-        ])
+        ]) # List of urls 
 nest_asyncio.apply()
 loader.requests_per_second = 1
-docs =  loader.aload()
+docs =  loader.aload() # REading web pages
 # print(docs)
 
 # print(f"Loaded {len(docs)} documents")
@@ -27,23 +27,23 @@ docs =  loader.aload()
     # print(f"--- Document {i} ---")
     # print(doc.page_content[:300])  # sample
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",task_type='QUESTION_ANSWERING')
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",task_type='QUESTION_ANSWERING') # Embadding model 
 
 
-embedding_dim = len(embeddings.embed_query("hello world"))
-test = (embeddings.embed_query("hello world"))
+embedding_dim = len(embeddings.embed_query("hello world")) # manually creating embedding dimensions
+# test = (embeddings.embed_query("hello world"))
 # print(f"Embedding dimension: {len(test)}")
 # print(f"Vector norm: {sum(x**2 for x in test) ** 0.5:.4f}")
-index = faiss.IndexFlatIP(embedding_dim)
+index = faiss.IndexFlatIP(embedding_dim) # Creating  FAISS vector DB index
 
 vector_store = FAISS(
     embedding_function=embeddings,
     index=index,
     docstore=InMemoryDocstore(),
     index_to_docstore_id={},
-)
+) 
 
-vector_store.add_documents(docs)
-vector_store.save_local("faiss_store")
+vector_store.add_documents(docs) # Adding docs in vector store 
+vector_store.save_local("faiss_store") # Saving faiss index and file locally.
 
 
